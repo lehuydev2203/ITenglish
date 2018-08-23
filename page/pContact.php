7ff0138f8b37ea3d47ee88c07add93b724317e16
Lê Huy <41155784+lehuyroxyproduction@users.noreply.github.com>
2018-08-23 14:17:16 +0700

    update contact
    full desktop , chưa xong responsive

--- page/pContact.php
+++ page/pContact.php
@@ -8,38 +8,96 @@
 
 ?>
 <div id="contact" class="row">
-    <div id="mail" class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+    <div id="mail" class="col-xl-6 col-lg-12 col-md-12 col-sm-12">
         <div id="form-mail" >
             <form id="form" action="">
                 <div class="row">
-                    <div class="offset-xl-2"></div>
-                    <div class="col-xl-8">
-                        <h1 style="text-align: center;font-size: 70px;margin-bottom: 30px">Lời Nhắn</h1>
+                    <div class="offset-xl-2 offset-lg-1 "></div>
+                    <div class="col-xl-8 col-lg-10 col-md-12 col-sm-12">
+                        <h1>Lời Nhắn</h1>
 
                         <h5>Tên :</h5>
                         <div class="style-input" >
                             <input type="text" required>
                         </div>
-                        <br>
+
                         <h5>Email :</h5>
                         <div class="style-input">
                             <input type="email" required>
                         </div>
-                        <br>
+
                         <h5>Lời Nhắn :</h5>
+                        <div id="style-textarea" style="width: 100%;border-radius: 10px;background-color: white">
                         <textarea name="" id="note"></textarea>
-                        <br>
+                        </div>
+
                         <div class="style-button">
                             <button type="reset" class="send" >Xóa</button> <button type="submit" class="send" >Gửi</button>
                         </div></div>
-                    <div class="offset-xl-2"></div>
+                    <div class="offset-xl-2 offset-lg-1"></div>
                 </div>
             </form>
         </div>
     </div>
-    <div id="comment" class="col-xl-6 col-lg-6 col-md-12 col-sm-12" style="padding: 20px 20px 20px 10px;" align="center">
-        <div id="form-comment" style="border: 1px solid white;border-radius: 10px;padding: 30px;">
-
+    <div id="comment" class="col-xl-6 col-lg-12 col-md-12 col-sm-12" align="center">
+        <div id="form-comment">
+            <div id="border">
+                <div id="commentbg"></div>
+                <div id="content-comment">
+                    <form id="form-comment-customer" action="" >
+                        <h5>Tên : </h5>
+                        <input type="text" class="form-control" style="outline:none" required>
+                        <h5 style="padding-top:5px">Comment : </h5>
+                        <textarea name="" id="box-comment" style=""></textarea>
+                        <button type="submit" class="btn btn-light btn-outline-light">Bình Luận</button>
+                    </form>
+                    <div id="result">
+                        <div id="outborder" >
+                            <div id="inborder">
+                                <div class="customer-comment">
+                                        <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-3" style="padding:0"><img src="img/logoMonster&taurus.png" width="100px" height="100px" alt=""></div>
+                                            <div class="col-9" style=" text-align: left">
+                                                <div class="title" style=""><h4><strong>{{Tên}}</strong> : <span>{{time}}</span></h4></div>
+                                                <div class="comment">
+                                                  {{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="customer-comment" style="border: 1px solid white;border-radius: 10px ;padding: 10px 0;margin-bottom: 10px;" >
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-3" style="padding:0"><img src="img/logoMonster&taurus.png" width="100px" height="100px" alt=""></div>
+                                            <div class="col-9" style=" text-align: left">
+                                                <div class="title" style="margin-bottom: 20px"><h4><strong>{{Tên}}</strong> : <span>{{time}}</span></h4></div>
+                                                <div class="comment">
+                                                  {{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="customer-comment" style="border: 1px solid white;border-radius: 10px ;padding: 10px 0;margin-bottom: 10px;" >
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-3" style="padding:0"><img src="img/logoMonster&taurus.png" width="100px" height="100px" alt=""></div>
+                                            <div class="col-9" style=" text-align: left">
+                                                <div class="title" style="margin-bottom: 20px"><h4><strong>{{Tên}}</strong> : <span>{{time}}</span></h4></div>
+                                                <div class="comment">
+                                                  {{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}{{bình luận}}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -49,21 +107,188 @@
 <script>
     var full="100%";
     var w_90="90%";
+    var w_80="80%";
     var height=(window.innerHeight)-80*2;
-    document.getElementById("mail").style.height=height+"px";
-    document.getElementById("mail").style.width=full;
-    document.getElementById("comment").style.height=height+"px";
-    document.getElementById("comment").style.width=full;
-    document.getElementById("form-mail").style.height=height-40+"px";
-    document.getElementById("form-mail").style.width=w_90;
-    document.getElementById("form-comment").style.height=height-40+"px";
-    document.getElementById("form-comment").style.width=w_90;
-    document.getElementById("note").style.height="400px";
-    var from_mail=document.getElementById("form-mail");
-    var h_form_mail=from_mail.clientHeight;
-    var from=document.getElementById("form");
-    var h_form=from.clientHeight;
-    document.getElementById("form-mail").style.paddingTop=(h_form_mail-h_form)/2+"px";
+    var x=window.innerWidth;
+    var y=window.innerHeight;
+    // alert(x);
+    if(x<y){
 
+    }else
+        if(x<=1073){
+            document.getElementById("mail").style.height=height+"px";
+            document.getElementById("mail").style.width=full;
+            document.getElementById("comment").style.height=height+"px";
+            document.getElementById("comment").style.width=full;
+            document.getElementById("form-mail").style.height=height-40+"px";
+            document.getElementById("form-mail").style.width=w_90;
+            document.getElementById("form-comment").style.height=height-40+"px";
+            document.getElementById("form-comment").style.width=w_90;
+            document.getElementById("style-textarea").style.height=height/4+20+"px";
+            document.getElementById("note").style.height=height/4+"px";
+            var from_mail=document.getElementById("form-mail");
+            var h_form_mail=from_mail.clientHeight;
+            var from=document.getElementById("form");
+            var h_form=from.clientHeight;
+            document.getElementById("form-mail").style.paddingTop=(h_form_mail-h_form)/2+"px";
+            //định dạng form comment
+            document.getElementById("border").style.width=full;
+            document.getElementById("border").style.height=full;
+            ////lấy bề ngang của #border
+            var border=document.getElementById("border");
+            var w_border=border.clientWidth;
+            var commentbg=document.getElementById("commentbg");
+            var w_commentbg=commentbg.clientWidth;
+            document.getElementById("commentbg").style.width=full;
+            document.getElementById("form-comment-customer").style.height="50%";
+            document.getElementById("form-comment-customer").style.padding="10px";
+            document.getElementById("box-comment").style.height="30%";
+            var h_border=border.clientHeight;
+            var form_comment_customer=document.getElementById("form-comment-customer");
+            var h_form_comment_customer=form_comment_customer.clientHeight;
+            document.getElementById("outborder").style.height=h_border-h_form_comment_customer-20+"px";
+            document.getElementById("content-comment").style.width=full;
+
+            var outborder=document.getElementById("outborder");
+            var inborder=document.getElementById("inborder");
+            var h_outborder=outborder.clientHeight;
+            var h_inborber=inborder.clientHeight;
+            if(h_outborder<h_inborber){
+                document.getElementById("outborder").style.overflowY="scroll";
+            }else{
+                document.getElementById("outborder").style.overflowY="hidden";
+            }
+        }else
+        if(x<=1137){
+            document.getElementById("mail").style.height=height+"px";
+            document.getElementById("mail").style.width=full;
+            document.getElementById("comment").style.height=height+"px";
+            document.getElementById("comment").style.width=full;
+            document.getElementById("form-mail").style.height=height-40+"px";
+            document.getElementById("form-mail").style.width=w_90;
+            document.getElementById("form-comment").style.height=height-40+"px";
+            document.getElementById("form-comment").style.width=w_90;
+            document.getElementById("style-textarea").style.height=height/4+20+"px";
+            document.getElementById("note").style.height=height/4+"px";
+            var from_mail=document.getElementById("form-mail");
+            var h_form_mail=from_mail.clientHeight;
+            var from=document.getElementById("form");
+            var h_form=from.clientHeight;
+            document.getElementById("form-mail").style.paddingTop=(h_form_mail-h_form)/2+"px";
+            //định dạng form comment
+            document.getElementById("border").style.width=full;
+            document.getElementById("border").style.height=full;
+            ////lấy bề ngang của #border
+            var border=document.getElementById("border");
+            var w_border=border.clientWidth;
+            var commentbg=document.getElementById("commentbg");
+            var w_commentbg=commentbg.clientWidth;
+            document.getElementById("commentbg").style.width=full;
+            document.getElementById("form-comment-customer").style.height="40%";
+            document.getElementById("form-comment-customer").style.padding="10px";
+            document.getElementById("box-comment").style.height="30%";
+            var h_border=border.clientHeight;
+            var form_comment_customer=document.getElementById("form-comment-customer");
+            var h_form_comment_customer=form_comment_customer.clientHeight;
+            document.getElementById("outborder").style.height=h_border-h_form_comment_customer-20+"px";
+            document.getElementById("content-comment").style.width=full;
+
+            var outborder=document.getElementById("outborder");
+            var inborder=document.getElementById("inborder");
+            var h_outborder=outborder.clientHeight;
+            var h_inborber=inborder.clientHeight;
+            if(h_outborder<h_inborber){
+                document.getElementById("outborder").style.overflowY="scroll";
+            }else{
+                document.getElementById("outborder").style.overflowY="hidden";
+            }
+        }else
+        if(x<=1422){
+            document.getElementById("mail").style.height=height+"px";
+            document.getElementById("mail").style.width=full;
+            document.getElementById("comment").style.height=height+"px";
+            document.getElementById("comment").style.width=full;
+            document.getElementById("form-mail").style.height=height-40+"px";
+            document.getElementById("form-mail").style.width=w_90;
+            document.getElementById("form-comment").style.height=height-40+"px";
+            document.getElementById("form-comment").style.width=w_90;
+            document.getElementById("style-textarea").style.height=height/4+20+"px";
+            document.getElementById("note").style.height=height/4+"px";
+            var from_mail=document.getElementById("form-mail");
+            var h_form_mail=from_mail.clientHeight;
+            var from=document.getElementById("form");
+            var h_form=from.clientHeight;
+            document.getElementById("form-mail").style.paddingTop=(h_form_mail-h_form)/2+"px";
+            //định dạng form comment
+            document.getElementById("border").style.width=full;
+            document.getElementById("border").style.height=full;
+            document.getElementById("commentbg").style.width=w_80;
+            ////lấy bề ngang của #border
+            var border=document.getElementById("border");
+            var w_border=border.clientWidth;
+            var commentbg=document.getElementById("commentbg");
+            var w_commentbg=commentbg.clientWidth;
+            document.getElementById("commentbg").style.marginLeft=(w_border-w_commentbg)/2+"px";
+            document.getElementById("form-comment-customer").style.height="40%";
+            document.getElementById("form-comment-customer").style.padding="10px";
+            document.getElementById("box-comment").style.height="35%";
+            var h_border=border.clientHeight;
+            var form_comment_customer=document.getElementById("form-comment-customer");
+            var h_form_comment_customer=form_comment_customer.clientHeight;
+            document.getElementById("outborder").style.height=h_border-h_form_comment_customer-20+"px";
+            var outborder=document.getElementById("outborder");
+            var inborder=document.getElementById("inborder");
+            var h_outborder=outborder.clientHeight;
+            var h_inborber=inborder.clientHeight;
+            if(h_outborder<h_inborber){
+                document.getElementById("outborder").style.overflowY="scroll";
+            }else{
+                document.getElementById("outborder").style.overflowY="hidden";
+            }
+        }else
+        if(x>1422){
+            document.getElementById("mail").style.height=height+"px";
+            document.getElementById("mail").style.width=full;
+            document.getElementById("comment").style.height=height+"px";
+            document.getElementById("comment").style.width=full;
+            document.getElementById("form-mail").style.height=height-40+"px";
+            document.getElementById("form-mail").style.width=w_90;
+            document.getElementById("form-comment").style.height=height-40+"px";
+            document.getElementById("form-comment").style.width=w_90;
+            document.getElementById("style-textarea").style.height=height/4+20+"px";
+            document.getElementById("note").style.height=height/4+"px";
+            var from_mail=document.getElementById("form-mail");
+            var h_form_mail=from_mail.clientHeight;
+            var from=document.getElementById("form");
+            var h_form=from.clientHeight;
+            document.getElementById("form-mail").style.paddingTop=(h_form_mail-h_form)/2+"px";
+            //định dạng form comment
+            document.getElementById("border").style.width=w_80;
+            document.getElementById("border").style.height=full;
+            document.getElementById("commentbg").style.width=w_80;
+            ////lấy bề ngang của #border
+            var border=document.getElementById("border");
+            var w_border=border.clientWidth;
+            var commentbg=document.getElementById("commentbg");
+            var w_commentbg=commentbg.clientWidth;
+            document.getElementById("commentbg").style.marginLeft=(w_border-w_commentbg)/2+"px";
+            document.getElementById("form-comment-customer").style.height="35%";
+            document.getElementById("form-comment-customer").style.padding="20px";
+            document.getElementById("form-comment-customer").style.padding="20px";
+            var h_border=border.clientHeight;
+            var form_comment_customer=document.getElementById("form-comment-customer");
+            var h_form_comment_customer=form_comment_customer.clientHeight;
+            document.getElementById("outborder").style.height=h_border-h_form_comment_customer-20+"px";
+
+            var outborder=document.getElementById("outborder");
+            var inborder=document.getElementById("inborder");
+            var h_outborder=outborder.clientHeight;
+            var h_inborber=inborder.clientHeight;
+            if(h_outborder<h_inborber){
+                document.getElementById("outborder").style.overflowY="scroll";
+            }else{
+                document.getElementById("outborder").style.overflowY="hidden";
+            }
+        }
 
 </script>
